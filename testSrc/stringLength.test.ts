98bb4fdeeafa018b1867658e315a674a50e3bc67
repinvv <repin.vladv@gmail.com>
2018-06-27 00:46:58 +0300

    16 tests

---
+++ testSrc/stringLength.test.ts
@@ -0,0 +1,81 @@
+import { expect } from "chai";
+import { maxLength, minLength, exactLength } from "../src";
+
+describe("maxLength", function (): void {
+  it("should return valid for shorter string", function (): void {
+    // arrange
+
+    // act
+    const result = maxLength(5)("abhy");
+
+    // assert
+    expect(result._valid).equals(true);
+    expect(result._maxLength).equals(5);
+  });
+
+  it("should return invalid for longer string", function (): void {
+    // arrange
+
+    // act
+    const result = maxLength(5)("asdf2213");
+
+    // assert
+    expect(result._valid).equals(false);
+  });
+});
+
+
+describe("minLength", function (): void {
+  it("should return invalid for shorter string", function (): void {
+    // arrange
+
+    // act
+    const result = minLength(5)("abhy");
+
+    // assert
+    expect(result._valid).equals(false);
+  });
+
+  it("should return valid for longer string", function (): void {
+    // arrange
+
+    // act
+    const result = minLength(5)("asdf2213");
+
+    // assert
+    expect(result._valid).equals(true);
+  });
+});
+
+
+describe("exactLength", function (): void {
+  it("should return invalid for shorter string", function (): void {
+    // arrange
+
+    // act
+    const result = exactLength(5)("abhy");
+
+    // assert
+    expect(result._valid).equals(false);
+  });
+
+  it("should return invalid for longer string", function (): void {
+    // arrange
+
+    // act
+    const result = exactLength(5)("abhy123");
+
+    // assert
+    expect(result._valid).equals(false);
+  });
+
+  it("should return valid for string of length", function (): void {
+    // arrange
+
+    // act
+    const result = exactLength(5)("asdf2");
+
+    // assert
+    expect(result._valid).equals(true);
+  });
+});
---
+++ testSrc/strings.test.ts
@@ -0,0 +1,104 @@
+import { expect } from "chai";
+import { testMessage } from "./testMessage";
+import { numeric, alphanumeric, alpha, regex } from "../src";
+
+describe("numeric", function (): void {
+  it("should return valid for number", function (): void {
+    // arrange
+
+    // act
+    const result = numeric(1);
+
+    // assert
+    expect(result._valid).equals(true);
+  });
+
+  it("should return valid for digits", function (): void {
+    // arrange
+
+    // act
+    const result = numeric("234");
+
+    // assert
+    expect(result._valid).equals(true);
+  });
+
+  it("should return invalid for letters", function (): void {
+    // arrange
+
+    // act
+    const result = numeric("asdf2213", testMessage);
+
+    // assert
+    expect(result._valid).equals(false);
+    expect(result._messages).deep.equals(["validation.numeric"]);
+  });
+});
+
+describe("alpha", function (): void {
+  it("should return valid for letters", function (): void {
+    // arrange
+
+    // act
+    const result = alpha("abhy");
+
+    // assert
+    expect(result._valid).equals(true);
+  });
+
+  it("should return invalid for digits", function (): void {
+    // arrange
+
+    // act
+    const result = alpha("asdf2213", testMessage);
+
+    // assert
+    expect(result._valid).equals(false);
+    expect(result._messages).deep.equals(["validation.alpha"]);
+  });
+});
+
+describe("alphanumeric", function (): void {
+  it("should return valid for letters and digits", function (): void {
+    // arrange
+
+    // act
+    const result = alphanumeric("abhy123");
+
+    // assert
+    expect(result._valid).equals(true);
+  });
+
+  it("should return invalid for symbols", function (): void {
+    // arrange
+
+    // act
+    const result = alphanumeric("asd&f22$13", testMessage);
+
+    // assert
+    expect(result._valid).equals(false);
+    expect(result._messages).deep.equals(["validation.alphanumeric"]);
+  });
+});
+
+describe("regex", function (): void {
+  it("should return valid for match", function (): void {
+    // arrange
+
+    // act
+    const result = regex(/^[a-c]*$/)("abc");
+
+    // assert
+    expect(result._valid).equals(true);
+  });
+
+  it("should return invalid for non-match", function (): void {
+    // arrange
+
+    // act
+    const result = regex(/^[a-c]*$/)("cde");
+
+    // assert
+    expect(result._valid).equals(false);
+  });
+});
