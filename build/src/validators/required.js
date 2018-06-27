c080454544dfe469be5bffef7a5ef35850a5be1c
repinvv <repin.vladv@gmail.com>
2018-06-28 00:30:17 +0300

    appveyor config

--- build/src/validators/required.js
+++ build/src/validators/required.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const makeMessage_1 = require("../makeMessage");
 function required(obj, message) {
-    const _valid = obj !== null && obj !== undefined && obj !== "";
+    const _valid = obj !== null && obj !== undefined && obj !== "" && obj !== [];
     return Object.assign({}, makeMessage_1.makeMessage(_valid, message, "validation.required"), { _required: true });
 }
 exports.required = required;
